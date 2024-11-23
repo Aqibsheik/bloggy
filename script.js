07bf235c57d6a86520c869c380df52b82065a8b9
Aqibsheik <aqibsheik5@gmail.com>
2024-11-23 10:53:02 +0530

    add button

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //this is scripted file
+
+    let Button = document.getElementById("btn");
+    let Contact = document.getElementById("contact");
+    let skills = document.querySelectorAll(".skl");
+
+    Button.addEventListener('click', function() {
+        Contact.textContent = `Contact: 8431843414 Email: aqibsheik5@gmail.com`;
+
+    })
+
+    
+
+
+skills.forEach(function(skill) {
+    skill.addEventListener('mouseover', function() {
+        skill.style.color = "green";
+    });
+});
+
+skills.forEach(function(skill) {
+    skill.addEventListener('mouseout', function() {
+        skill.style.color = "";
+    });
+});
+
+
